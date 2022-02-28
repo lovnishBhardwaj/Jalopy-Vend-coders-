4c984761c134f6fb49f600cdbbf92d93ade9911e
lovnishBhardwaj <76565429+lovnishBhardwaj@users.noreply.github.com>
2022-02-28 13:44:52 +0530

    Add files via upload

--- resp.js
+++ resp.js
@@ -7,3 +7,25 @@ burger.addEventListener('click', ()=>{
      navbar.classList.toggle('h-nav-resp');
      navList.classList.toggle('v-class-resp');
 })
+
+function getVal(){
+  const val = document.querySelector('input').value;
+  console.log(val);
+}
+
+$(document).ready(function(){
+     $("form").submit(function(){
+          name = $("input[name='name']").val();
+          phone = $("input[name='phone']").val();
+          email = $("input[name='email']").val();
+          subject = $("input[name='subject']").val();
+          text = $("textarea[name='text']").val();
+
+          document.writeln(name);
+          document.writeln(phone);
+          document.writeln(email);
+          document.writeln(subject);
+          document.writeln(text);
+          return false;
+     })
+})
